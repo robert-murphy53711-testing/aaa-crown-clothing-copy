b33188bca44601a5f7bb67e574f196cd0e82b134
Manual QA <qatech@Manuals-Air.attlocal.net>
2024-05-27 21:29:36 -0400

    initial upload

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,44 @@
+import {Fragment, useContext} from 'react';
+import {Outlet, Link} from 'react-router-dom';
+
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
+import {UserContext} from '../../contexts/user.context';
+import './navigation.styles.scss';
+
+import {signOutUser} from '../../utils/firebase/firebase.utils'
+
+const Navigation = () => {
+
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    console.log( currentUser );
+
+    const signOutHandler = async () => {
+      const response = await signOutUser();
+      //setCurrentUser(null);
+    }
+
+    return (
+      <Fragment>
+        <div className="navigation">
+          <Link className='logo-container' to='/'> 
+            <CrwnLogo className='logo' />
+          </Link>
+          <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>
+              SHOP
+            </Link>
+            { currentUser != null ? <span className='nav-link' onClick={ signOutHandler }>SIGN OUT</span> : (
+              
+            <Link className='nav-link' to='auth'>
+              SIGN IN
+          </Link>
+            )}
+          </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    );
+}
+
+export default Navigation;
