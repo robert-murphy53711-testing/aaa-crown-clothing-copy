0e8220d7a983dfd219c9c628edd13a19000dc8f0
robert.murphy53711.testing <robert.murphy53711.testing@gmail.com>
2024-09-02 14:22:51 -0400

    Added App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Authentication from './routes/authentication/authentication.component';
 //import CartCheckout from './components/cart-checkout/cart-checkout.component';
 //import CartCheckout2 from './components/cart-checkout2/cart-checkout2.component';
 //import CartCheckout3 from './components/cart-checkout3/cart-checkout3.component';
-import CartCheckout4 from './components/cart-checkout4/cart-checkout4.component';
+//import CartCheckout4 from './components/cart-checkout4/cart-checkout4.component';
+import Checkout from './routes/checkout/checkout.component';
 import Shop from './routes/shop/shop.component';
 
 
@@ -28,7 +29,7 @@ const  App = () => {
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop />} />
           <Route path="auth" element={<Authentication />} />
-          <Route path='checkout' element={<CartCheckout4 />} />
+          <Route path='checkout' element={<Checkout />} />
         </Route>
       </Routes>
   );
